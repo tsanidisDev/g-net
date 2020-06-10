82f5d6d7e5af5bdfe57e428ef5f766b4732fbca4
Petros <petros@vervesearch.com>
2020-06-10 17:38:21 +0100

    fix theme setup, style/animate loader svg

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,21 +4,38 @@ export interface LoaderProps {
     className?: string;
 }
 
+function generateCircles(count, space) {
+    var circlesArr = [];
+    for (var i = 0; i < count; i++) {
+        circlesArr.push(
+            <circle
+                key={"load-circle-" + i}
+                className={"ldr__cr"}
+                cx={`${12.5 + (i * space)}`}
+                cy="12.5"
+                fill="#0EF907" r="12.5"
+                style={{
+                    animationDelay: `${i * 0.25 * 0.5}s, ${i * 0.25 * 0.5}s`
+                }}
+            />);
+    }
+
+    return circlesArr;
+}
+
 export const Loader: React.SFC<LoaderProps> = (props) => {
     const cls = props.className || "";
 
     return (
-        <div className={"loader " + cls}>
+        <div className={"ldr " + cls}>
             <svg
-                height="25"
-                width="109"
+                height="150"
+                width="150"
                 fill="none"
                 viewBox="0 0 109 25"
                 xmlns="http://www.w3.org/2000/svg"
             >
-                <circle cx="12.5" cy="12.5" fill="#0EF907" r="12.5" />
-                <circle cx="54.5" cy="12.5" fill="#0EF907" r="12.5" />
-                <circle cx="96.5" cy="12.5" fill="#0EF907" r="12.5" />
+                {generateCircles(3, 42)}
             </svg>
         </div>
     )
